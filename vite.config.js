75f37c962d5923550c4bebad0e6ba087cf6cf2ca
Rishirajbh <rishiraj7b9@gmail.com>
2026-05-23 17:01:16 +0530

    folder path updated

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: "./src/index.js",
-      name: "CurrencyBreakdown",
+      name: "CurrencyBreakdownUI",
       fileName: "currency-breakdown-ui",
     },
   },
